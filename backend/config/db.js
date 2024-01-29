5bf22d9fa061c82c0da73d8b7592889123d0cc79
diayaya1999@gmail.com <diayaya1999@gmail.com>
2024-01-29 17:10:14 +0000

    first commit

---
+++ backend/config/db.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+module.exports = () => {
+  const connectionParams ={
+    useNewUrlParser:true,
+    useUnifiedTopology: true,
+  };
+   try {
+    mongoose.connect(process.env.MONGO_URL, connectionParams);
+    console.log('Base de donnée connectée');
+   } catch(error) {
+    console.log(error);
+    console.log("pas connecté");
+   }
+
+};
+ 
---
+++ backend/server.js
@@ -0,0 +1,72 @@
+// const express = require("express");
+// // const connectDB = require("./config/db");
+// const EmployeeModel = require('./models/Employee')
+// // const port = process.env.PORT || 5000; // Utiliser le port défini dans les variables d'environnement ou le port 5000 par défaut
+// require('dotenv').config();
+// // Connexion à la DB
+// // connectDB();
+
+// const app = express();
+
+// // Middleware qui permet de traiter les données de la Request
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+
+// // app.use("/post", require("./routes/post.routes"));
+// mongoose
+//   .connect("mongodb+srv://jahreuf:Djilo13dia@app-test.1kg1yqb.mongodb.net/employee", {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   })
+//   .then(() => {
+//     console.log("base de donnees connecté");
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
+// app.post("/register", (req, res) => {
+//   EmployeeModel.create(req.body)
+//     .then((employees) => res.json(employees))
+//     .catch((err) => {
+//       console.error(err);
+//       res.status(500).json({ error: "Internal Server Error" });
+//     });
+// });
+
+// // Lancer le serveur
+// app.listen(3000, () => console.log("Le serveur a démarré au port " + 3000));
+const express = require ('express')
+const bodyParser = require("body-parser");
+
+const app = express();
+const cors = require ('cors');
+
+require('dotenv').config();
+const connection = require("./config/db");
+const userRoutes = require ('./routes/users');
+const authRoutes = require ("./routes/auth");
+
+// database connection
+connection();
+
+app.use(express.json());
+app.use(cors());
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
+
+app.use("/api/users",userRoutes)
+app.use("/api/users", authRoutes);
+
+
+// // Définir un dossier pour servir des fichiers statiques (images)
+// const hotelRoutes = require("./routes/hotelRoutes");
+// app.use("/hotels", hotelRoutes);
+
+// // Définir un dossier pour servir des fichiers statiques (images)
+// app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+
+const port = process.env.PORT || 8080; // Utiliser le port défini dans les variables d'environnement ou le port 5000 par défaut
+app.listen(port, () => console.log(`backListening on port ${port}...`));
